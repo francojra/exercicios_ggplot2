c17e8234520d95865124a4dba943941ed6be15df
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-07 23:32:09 -0300

    Add files via upload

---
+++ exercicios_ggplot2.R
@@ -0,0 +1,16 @@
+
+# Exercícios - Produção de gráficos com o pacote ggplot2 ----------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 07/09/2022 -------------------------------------------------------------------------------------------------------------------------
+
+# Exercícios -------------------------------------------------------------------------------------------------------------------------------
+
+# 1 - Escolha um conjunto de dados disponível no R
+# 2 - Carregue os pacotes necessários
+# 3 - Produza os seguintes gráficos:
+# a) Gráfico de histograma e densidade
+# b) Gráfico de boxplot e violino
+# c) Gráfico de barras com valores de média e erro padrão
+# d) Gráfico de linhas
+
+
