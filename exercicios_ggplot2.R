65e85db46e11342c90b0ea2888e0d16b0cab1f61
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-07 23:44:05 -0300

    Add files via upload

--- exercicios_ggplot2.R
+++ exercicios_ggplot2.R
@@ -8,8 +8,8 @@
 # 1 - Escolha um conjunto de dados disponível no R
 # 2 - Carregue os pacotes necessários
 # 3 - Produza os seguintes gráficos:
-# a) Gráfico de histograma e densidade
-# b) Gráfico de boxplot e violino
+# a) Gráficos de histograma e densidade
+# b) Gráficos de boxplot e violino
 # c) Gráfico de barras com valores de média e erro padrão
 # d) Gráfico de linhas
 
@@ -21,3 +21,29 @@ library(dplyr) # Pacote que carrega o banco de dados Starwars
 dados <- starwars
 View(dados)
 
+# Exercício 2
+
+library(tidyverse)
+
+# Exercício 3
+
+# a) Gráficos de histograma e densidade
+
+dados <- dados %>%
+  drop_na(mass) %>%
+  view()
+
+ggplot(dados, aes(x = mass)) +
+  geom_histogram(bins = 30)
+
+ggplot(dados, aes(x = mass, y = height)) +
+  geom_density_2d()
+
+ggplot(dados, aes(x = mass, y = height)) +
+  geom_density_2d_filled()
+
+ggplot(dados, aes(x = mass, y = height)) +
+  geom_density_2d_filled()
+
+ggplot(dados, aes(x = mass)) +
+  geom_density()
